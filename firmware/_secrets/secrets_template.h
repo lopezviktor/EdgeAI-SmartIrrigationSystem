3d1658c71b7ba7f290d4e1e00f5fe60a5636d04f
Victor <victorlopezlopez93@gmail.com>
2025-10-23 14:45:02 +0100

    Add public secrets_template.h for credentials configuration

---
+++ firmware/_secrets/secrets_template.h
@@ -0,0 +1,6 @@
+#pragma once
+// RENAME this file to secrets.h and fill with your real credentials.
+// Do NOT commit secrets.h to Git.
+#define WIFI_SSID        "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD    "YOUR_WIFI_PASSWORD"
+#define THINGSPEAK_KEY   "YOUR_WRITE_API_KEY"
